746ee34ebc2200c3b797fb38340b4238258d4a33
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-03 03:11:56 -0500

    Tailwind  css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,32 +7,62 @@ const socket = io("http://localhost:4000");
 
 function App() {
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    // {
+    //   body: "Message test",
+    //   from: "user1",
+    // },
+  ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     socket.emit("message", message);
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
     setMessage("");
   };
 
   useEffect(() => {
-    const receiveMessage = (message) => console.log(message);
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+    };
 
     socket.on("message", receiveMessage);
 
     return () => {
       socket.off("message", receiveMessage);
     };
-  }, []);
+  }, [messages]);
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
         <input
           type="text"
           onChange={(e) => setMessage(e.target.value)}
           value={message}
+          className="border-2 border-zinc-500 p-2 w-full"
         />
-        <button>send</button>
+        {/* <button className="bg-blue-500">send</button> */}
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+              }`}
+            >
+              <p>
+                {message.from}: {message.body}
+              </p>
+            </li>
+          ))}
+        </ul>
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,30 +2,33 @@ import express from "express";
 import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
 import http from "http";
-import cors from 'cors';
+import cors from "cors";
 
-import { PORT } from './config.js';
+import { PORT } from "./config.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: 'http://localhost:3000',
-  }
+    origin: "http://localhost:3000",
+  },
 });
 
 app.use(cors());
 app.use(morgan("dev"));
 
-io.on('connection', (socket) => {
-  console.log(socket.id);
+io.on("connection", (socket) => {
+  // console.log(socket.id);
   // console.log("a user started a connection");
 
-  socket.on("message", message => {
-    console.log(message);
-    socket.broadcast.emit("message", message);
-  })
-})
+  socket.on("message", (message) => {
+    // console.log(message);
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
 
 server.listen(PORT, () => {
   console.log("Server is running on port", PORT);
